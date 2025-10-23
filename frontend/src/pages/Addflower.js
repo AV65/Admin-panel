c508834af4f8cf3ac4e1981d2f1dcc602fb5c90f
Muhammad Farouk <muhammadfarouk8428@gmail.com>
2025-10-23 12:37:46 -0700

    edited Addflower.css name

--- frontend/src/pages/Addflower.js
+++ frontend/src/pages/Addflower.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./AddFlower.css";
+import "./Addflower.css";
 
 const AddFlower = () => {
   const [Name, setName] = useState("");
